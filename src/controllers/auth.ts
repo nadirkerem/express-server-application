d723e132f767ef661abdc197cfb6ea0930f7913d
Nadir Kerem Cetin <nadirkerem12@gmail.com>
2024-07-11 18:54:42 -0400

    hash user password with bcrypt

--- src/controllers/auth.ts
+++ src/controllers/auth.ts
@@ -1,32 +1,47 @@
 import { Request, Response, NextFunction } from 'express';
+import bcrypt from 'bcrypt';
+
 import { User } from '../interfaces/user';
 
 export let users: User[] = [];
 let currentUserId = 1;
 
-export const register = (req: Request, res: Response, next: NextFunction) => {
+// generateToken function...
+
+export const register = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
   const { username, password } = req.body;
   if (!username || !password) {
     next({ status: 400, message: 'Username and password are required' });
     return;
   }
-  const newUser: User = { id: currentUserId++, username, password };
+  const hashedPassword = await bcrypt.hash(password, 10);
+  const newUser: User = {
+    id: currentUserId++,
+    username,
+    password: hashedPassword,
+  };
   users.push(newUser);
-  res.status(201).json({ message: 'User registered', user: newUser });
+  res.json({ message: 'User registered' });
 };
 
-export const login = (req: Request, res: Response, next: NextFunction) => {
+export const login = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
   const { username, password } = req.body;
   if (!username || !password) {
     next({ status: 400, message: 'Username and password are required' });
     return;
   }
-  const user = users.find(
-    (u) => u.username === username && u.password === password
-  );
-  if (!user) {
+  const user = users.find((u) => u.username === username);
+  if (user && (await bcrypt.compare(password, user.password))) {
+    res.json({ message: 'User logged in' });
+  } else {
     next({ status: 401, message: 'Invalid credentials' });
-    return;
   }
-  res.json({ message: 'User logged in', user });
 };
